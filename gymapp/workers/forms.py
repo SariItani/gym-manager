5cfd4908d18b99efc6a1f3fd1d3125de48490e75
SariItani <74187768+SariItani@users.noreply.github.com>
2022-11-25 22:42:21 +0200

    employee database
    now the employee database is working

--- gymapp/workers/forms.py
+++ gymapp/workers/forms.py
@@ -1,15 +1,11 @@
 from django import forms
 from .models import *
 
-class Signup(forms.Form):
-    username = forms.CharField(label="username", max_length=20)
-    password = forms.PasswordInput()
-    email = forms.EmailField()
-    gender = forms.ChoiceField(choices=("Male", "Female"))
-    position = forms.CharField(max_length=50)
-
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    class Meta:
-        model = Customers
-        
+class Create(forms.Form):
+    name = forms.CharField(label="Employee name", max_length=20)
+    email = forms.EmailField(label="Email", max_length=30)
+    password = forms.CharField(label="password", max_length=20)
+    nb = forms.IntegerField(label="Employee ID")
+    jobtitle = forms.CharField(label="Job Title", max_length=30)
+    legal = forms.BooleanField(label="Legal")
+    salary = forms.IntegerField(label="Salary")
--- gymapp/workers/views.py
+++ gymapp/workers/views.py
@@ -1,24 +1,50 @@
 from django.shortcuts import render, HttpResponseRedirect
-from .models import Schedule, Workers, Customers, Cams, Stock
+from .models import *
+from .forms import *
 
 # Create your views here.
+
+
 def index(response):
     return render(response, "workers/index.html", {})
+
 
 def customers(response):
     pass
 
+
 def schedule(response):
     pass
+
 
 def personal(response):
     pass
 
+
 def create(response):
-    return render(response, "workers/login.html", {})
+    if response.method == "POST":
+        form = Create(response.POST)
+        print(response.POST)
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            e = form.cleaned_data["email"]
+            p = form.cleaned_data["password"]
+            nb = form.cleaned_data["nb"]
+            j = form.cleaned_data["jobtitle"]
+            l = form.cleaned_data["legal"]
+            s = form.cleaned_data["salary"]
+            employee = Workers(name=n, email=e, password=p,
+                               nb=nb, jobtitle=j, legal=l, salary=s)
+            employee.save()
+            return HttpResponseRedirect("/work/personal/")
+    else:
+        form = Create()
+    return render(response, "workers/login.html", {"form": form})
+
 
 def cams(response):
     pass
+
 
 def stock(response):
     pass
