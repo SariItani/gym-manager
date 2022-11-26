1615e23ec4ad07feeca0858a673f79c91ad19f99
SariItani <74187768+SariItani@users.noreply.github.com>
2022-11-26 03:01:42 +0200

    login page

--- gymapp/gymbros/views.py
+++ gymapp/gymbros/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from datetime import datetime
 
 current_dateTime = str(datetime.now())
@@ -8,7 +9,14 @@ def index(response):
     return render(response, "gymbros/index.html", {})
 
 def login(response):
-    pass
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("login"):
+            n = response.POST.get("username")
+            p = response.POST.get("password")
+            # validate
+            return HttpResponseRedirect("/index")
+    return render(response, "gymbros/login.html", {})
 
 def signin(response):
     pass
